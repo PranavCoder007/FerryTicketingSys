5548886c4c383fa9c5206a53527d5b5f9bd814a9
Pranav Nair <148352137+PranavCoder007@users.noreply.github.com>
2024-11-22 12:14:54 +0530

    Update .eslintrc.js

--- build/npm/.eslintrc.js
+++ build/npm/.eslintrc.js
@@ -1,4 +1,16 @@
-// .eslintrc.js
-const config = require('./.eslintrc.json');
-
-module.exports = config;
+// .eslintrc.js (updated for flat config)
+module.exports = {
+  languageOptions: {
+    globals: {
+      browser: true, // Global variable for browser environment
+      es2021: true,   // Support for ES2021
+    },
+    parserOptions: {
+      ecmaVersion: 12, // Use ECMAScript 2021
+    },
+  },
+  rules: {
+    // You can add custom rules here
+  },
+  extends: ['eslint:recommended'],
+};
